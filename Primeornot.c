501d1c102c6300f2dac420ea51b020af6dbf620e
CHIKIRAPALLYNAGAJYOTHI <120176214+CHIKIRAPALLYNAGAJYOTHI@users.noreply.github.com>
2022-12-09 18:15:20 +0530

    Create Primeornot.c

---
+++ Primeornot.c
@@ -0,0 +1,25 @@
+#include<studio.h>
+#include<conio.h>
+Void main()
+{
+int n,c=0;
+Clrscr();
+Print f ("enter any number \n");
+Scanf("%d",&n);
+For(i=1,I<n,I++);
+{
+If(n%i==0)
+{
+C++;
+}
+}
+If (C==2)
+{
+Printf(it's prime ");
+}
+Else
+{
+Printf ("it's not prime ");
+getch()
+}
+}
